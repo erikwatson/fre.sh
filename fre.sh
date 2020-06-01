f0879596a46801a5f5f0db8aa26d4dd4f2b52d1f
Erik Watson <championchap@gmail.com>
2020-06-01 14:29:54 +0100

    commenting out the base_16 shell command, added a message for the user to do it manually post install

--- fre.sh
+++ fre.sh
@@ -51,6 +51,10 @@ ln -s ~/vimrc/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-## Base16 Shell
+# Base16 Shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-base16_tomorrow-night
+# base16_tomorrow-night - this doesn't work for some reason?
+
+
+echo ""
+echo 'You still need to run "base16_tomorrow-night" and set the terminal font to "Hack" yourself'
